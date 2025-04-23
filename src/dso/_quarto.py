543700467fdc106a2cafec317a8779564059a334
Gregor Sturm <gregor.sturm@boehringer-ingelheim.com>
2025-04-23 14:00:08 +0200

    Document shell completion (#116)
    * Document shell completion
    
    * update changelog
    
    * fix typo

--- src/dso/_quarto.py
+++ src/dso/_quarto.py
@@ -32,7 +32,7 @@ def render_quarto(quarto_dir: Path, report_dir: Path, before_script: str, cwd: P
     # clean up existing `.rmarkdown` files that may interfere with rendering
     # these are leftovers from a previous, failed `quarto render` attempt. If they still exist, the next attempt
     # fails. We remove them *before* the run instead of cleaning them up *after* the run, because they
-    # may be usefule for debugging failures.
+    # may be useful for debugging failures.
     # see https://github.com/Boehringer-Ingelheim/dso/issues/54
     for f in quarto_dir.glob("*.rmarkdown"):
         if f.is_file():
