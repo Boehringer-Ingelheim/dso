17c5c8b72908a25c609eec65ddccc6706a2da85f
Gregor Sturm <gregor.sturm@boehringer-ingelheim.com>
2025-04-25 13:24:11 +0200

    Refactor stage creation

--- src/dso/cli/_create.py
+++ src/dso/cli/_create.py
@@ -8,6 +8,7 @@ from importlib import resources
 from os import getcwd
 from pathlib import Path
 from textwrap import dedent, indent
+from typing import Literal
 
 import rich_click as click
 from rich.prompt import Confirm, Prompt
@@ -35,7 +36,7 @@ CREATE_STAGE_HELP_TEXT = dedent(
 )
 
 
-def _get_templates() -> dict[str, dict]:
+def _get_template_libraries() -> dict[str, dict]:
     """Get a list of stages from template library(s)
 
     Paths to template libraries are obtained from `DSO_TEMPLATE_LIBRARIES` env variable.
@@ -43,7 +44,7 @@ def _get_templates() -> dict[str, dict]:
     """
     lib_paths = set(os.environ.get("DSO_TEMPLATE_LIBRARIES", "dso.templates").split(":"))
 
-    templates = {}
+    libraries = {}
     for lib_path in lib_paths:
         try:
             template_module = importlib.import_module(lib_path)
@@ -54,42 +55,77 @@ def _get_templates() -> dict[str, dict]:
         with (tmp_dir / "index.json").open("rb") as f:
             index = json.load(f)
             id_ = index["id"]
-            if id_ in templates:
+            if id_ in libraries:
                 raise ValueError(f"ID {id_} is not unique for {lib_path}.")
-            templates[id_] = index
-            templates[id_]["path"] = lib_path
+            libraries[id_] = index
+            libraries[id_]["path"] = lib_path
+    return libraries
+
+
+def _get_templates(library: dict, type_: Literal["init", "folder", "stage"]) -> dict:
+    """
+    From a given template library index, get a specified template.
+
+    The template library index is one element in the dict obtained through `get_template_libraries).
+    """
+    templates = {}
+    for t in library[type_]:
+        if t["id"] in templates:
+            raise ValueError(f"ID {t['id']} is not unique for library {library['id']} and type {type_}.")
+        templates[t["id"]] = t
+
     return templates
 
 
-@click.option("--description")
-@click.option("--template", type=click.Choice(list(STAGE_TEMPLATES)))
-@click.option("--library", help="Choose the template library to use")
-@click.argument("name", required=False)
-@click.command("stage", help=CREATE_STAGE_HELP_TEXT)
-def dso_create_stage(
-    name: str | None = None, template: str | None = None, library: str | None = None, description: str | None = None
-):
+@click.option("--template", "-t", "template_id", type=click.Choice(list(STAGE_TEMPLATES)))
+@click.option("--library", "-l", "library_id", help="Choose the template library to use")
+@click.command(
+    "stage",
+    help=CREATE_STAGE_HELP_TEXT,
+    context_settings={
+        "ignore_unknown_options": True,
+        "allow_extra_args": True,
+    },
+)
+@click.pass_context
+def dso_create_stage(ctx, template_id: str | None = None, library_id: str | None = None):
     """Create a new stage."""
     import questionary
+    from questionary import Choice
 
     from dso._compile_config import compile_all_configs
 
-    templates = _get_templates()
+    # get extra arguments, see https://stackoverflow.com/questions/32944131/add-unspecified-options-to-cli-command-using-python-click
+    kwargs = {ctx.args[i][2:]: ctx.args[i + 1] for i in range(0, len(ctx.args), 2)}
 
-    if len(templates) == 1:
-        library = next(iter(templates))
-    if library is None:
-        library = str(questionary.select("Choose a template library:", choices=list(templates)).ask())
+    libraries = _get_template_libraries()
 
-    if template is None:
-        stage_template_ids = [x["id"] for x in templates[library]["stage"]]
-        template = str(questionary.select("Choose a template:", choices=list(stage_template_ids)).ask())
+    if len(libraries) == 1:
+        library_id = next(iter(libraries))
+    if library_id is None:
+        library_id = str(questionary.select("Choose a template library:", choices=list(libraries)).ask())
 
-    if name is None:
-        name = Prompt.ask('[bold]Please enter the name of the stage, e.g. "01_preprocessing"')
+    library = libraries[library_id]
+    templates = _get_templates(library, "stage")
 
-    if description is None:
-        description = Prompt.ask("[bold]Please add a short description of the stage")
+    if template_id is None:
+        choices = [Choice(t["id"], value=t["id"], description=t["description"]) for t in templates.values()]
+        template_id = str(
+            questionary.select(
+                "Choose a template:",
+                choices=choices,
+                use_jk_keys=False,
+                use_search_filter=True,
+                show_selected=True,
+                show_description=True,
+            ).ask()
+        )
+        template = templates[template_id]
+
+    for p in template["params"]:
+        name = p["name"]
+        if name not in kwargs:
+            kwargs[name] = questionary.text(p["description"]).ask()
 
     target_dir = Path(getcwd()) / name
     if target_dir.exists():
@@ -101,20 +137,16 @@ def dso_create_stage(
     project_root = get_project_root(target_dir)
     stage_path = target_dir.relative_to(project_root)
 
-    instantiate_template(
-        templates[library]["path"],
-        target_dir,
-        stage_name=name,
-        stage_description=description,
-        stage_path=stage_path,
-    )
+    instantiate_template(libraries[library_id]["path"], target_dir, rel_path_from_project_root=stage_path, **kwargs)
     log.info("[green]Stage created successfully.")
     compile_all_configs([target_dir])
 
 
 @click.argument("name", required=False)
 @click.command("folder")
-def dso_create_folder(name: str | None = None):
+@click.option("--library", help="Choose the template library to use")
+@click.option("--template", type=click.Choice(list(STAGE_TEMPLATES)))
+def dso_create_folder(name: str | None = None, template: str | None = None, library: str | None = None):
     """Create a new folder. A folder can contain subfolders or stages.
 
     Technically, nothing prevents you from just using `mkdir`. This command additionally adds some default
